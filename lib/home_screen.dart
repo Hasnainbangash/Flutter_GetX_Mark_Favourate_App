2f917baf3ecec428f405b2830d51b1055e7fc5de
Bangash38 <bangashmhasnain38@gmail.com>
2025-04-15 12:49:08 +0500

    Initial commit all files get commited

---
+++ lib/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GetX Tutorials'),
+      ),
+    );
+  }
+}
